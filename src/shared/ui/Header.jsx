1035427c58e6bc152c8f94318201826ee21dec1b
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-17 17:56:57 +0100

    feat(header): update reponsive layout for larger screens

--- src/shared/ui/Header.jsx
+++ src/shared/ui/Header.jsx
@@ -7,22 +7,35 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="flex justify-between items-center p-4 text-[hsl(var(--text-primary))]">
-      <div className="flex items-center gap-2">
-        <img src={Logo} alt="App logo" />
-        <h1 className="font-bold">TaxSense</h1>
-      </div>
+    <header className="flex justify-center md:p-8">
+      <div className="flex-1 flex justify-between items-center p-4 text-[hsl(var(--text-primary))] md:bg-white md:text-black md:px-6 md:rounded-full md:max-w-4xl">
+        <div className="flex items-center gap-2">
+          <img src={Logo} alt="App logo" />
+          <h1 className="font-bold text-md md:text-2xl">TaxSense</h1>
+        </div>
 
-      <div className="flex flex-col items-end relative">
-        <button
-          className={`cursor-pointer ${isOpen ? "ring-2 ring-[hsl(var(--color-accent))]" : ""} rounded-md p-0.5`}
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Open menu"
-        >
-          <Menu strokeWidth={3} />
-        </button>
+        <div className="flex flex-col items-end relative md:hidden">
+          <button
+            className={`cursor-pointer ${isOpen ? "ring-2 ring-[hsl(var(--color-accent))]" : ""} rounded-md p-0.5`}
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Open menu"
+          >
+            <Menu strokeWidth={3} />
+          </button>
 
-        {isOpen && <AuthMenu />}
+          {isOpen && <AuthMenu />}
+        </div>
+
+        <nav className="hidden md:block">
+          <ul className="flex items-center gap-4">
+            <li className="ring-2 py-2 px-4 rounded-full ring-[hsl(var(--color-accent))] cursor-pointer">
+              <a>Login</a>
+            </li>
+            <li className="bg-[hsl(280,100%,15%)] text-white py-2 px-4 rounded-full cursor-pointer">
+              <a>Sign Up</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
