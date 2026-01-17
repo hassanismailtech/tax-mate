b42c6cd59f7759a2bd84b325af2ab9686aaf75f1
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-17 11:17:53 +0100

    feat(pages/home): update homepage with details and secondary text styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,24 @@ import AskAiButton from "../../shared/ui/AskAiButton";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-around">
+    <main className="flex flex-1 flex-col items-center justify-center p-4">
       <AskAiButton />
+
+      <section className="flex flex-col gap-6">
+        <header className="px-2">
+          <h1 className="text-2xl text-center">
+            Understand{" "}
+            <span className="text-[hsl(var(--text-accent))]">your</span> taxes,
+            No <span className="text-[hsl(var(--text-accent))]">jargon</span>,
+            No fear
+          </h1>
+        </header>
+
+        <p className="text-lg text-center text-[hsl(var(--text-secondary))]">
+          Ask questions about Nigerian taxes in plain language-simple, friendly
+          and designed for everyone.
+        </p>
+      </section>
     </main>
   );
 }
