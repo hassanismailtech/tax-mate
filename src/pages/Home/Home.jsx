ef154fc2da7abeb684e4fabf80766a0b15fdd5eb
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-17 18:10:33 +0100

    feat(pages/home): make homepage layout responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,13 @@ import CtaButton from "../../shared/ui/CtaButton";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center gap-10 p-4">
+    <main className="flex flex-1 flex-col items-center justify-center gap-10 p-4 md:justify-start md:py-20">
       <AskAiButton />
 
       <section className="flex flex-col items-center gap-10">
         <div className="flex flex-col items-center gap-6">
           <header className="px-2">
-            <h1 className="text-2xl text-center">
+            <h1 className="text-3xl font-bold text-center md:text-5xl">
               Understand{" "}
               <span className="text-[hsl(var(--text-accent))]">your</span>{" "}
               taxes, No{" "}
@@ -18,7 +18,7 @@ export default function Home() {
             </h1>
           </header>
 
-          <p className="text-lg text-center text-[hsl(var(--text-secondary))]">
+          <p className="text-lg text-center text-[hsl(var(--text-secondary))] md:text-xl">
             Ask questions about Nigerian taxes in plain language-simple,
             friendly and designed for everyone.
           </p>
