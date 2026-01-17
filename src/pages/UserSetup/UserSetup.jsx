35877df7e9c8810472c7d29a0b3c008d01e2b4b1
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-18 00:42:55 +0100

    feat(pages/usersetup): add back navigation using useNavigate

--- src/pages/UserSetup/UserSetup.jsx
+++ src/pages/UserSetup/UserSetup.jsx
@@ -1,10 +1,21 @@
 import { ArrowLeft } from "lucide-react";
 import EmploymentTypeSelector from "../../shared/ui/EmploymentTypeSelector";
+import { useNavigate } from "react-router";
 
 export default function UserSetup() {
+  const navigate = useNavigate();
+
+  const handleclick = () => {
+    navigate(-1);
+  };
+
   return (
     <main className="p-4 flex flex-col gap-6">
-      <button className="text-[hsl(var(--color-accent))]" aria-label="Go back">
+      <button
+        className="text-[hsl(var(--color-accent))] cursor-pointer"
+        onClick={handleclick}
+        aria-label="Go back"
+      >
         <ArrowLeft />
       </button>
 
