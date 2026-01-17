dd607398ac538ae9bc9738547aa56c26b9631a2f
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-17 09:19:40 +0100

    feat(ui): update header with logo and layout changes

--- src/shared/ui/Header.jsx
+++ src/shared/ui/Header.jsx
@@ -1,12 +1,14 @@
 import { Menu } from "lucide-react";
+import Logo from "../assets/images/logo.svg";
 
 export default function Header() {
   return (
     <header className="flex justify-between p-4">
-      <div>
-        <h1>TaxSense</h1>
+      <div className="flex items-center gap-2">
+        <img src={Logo} alt="App logo" />
+        <h1 className="font-bold">TaxSense</h1>
       </div>
-      <Menu />
+      <Menu strokeWidth={3} />
     </header>
   );
 }
