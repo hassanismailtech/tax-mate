d43b14c5bc55a9224e89179e086907da617d86c8
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-01-17 12:52:36 +0100

    feat(shared/ui): implement toggle open/close functionality in the header

--- src/shared/ui/Header.jsx
+++ src/shared/ui/Header.jsx
@@ -1,8 +1,11 @@
 import { Menu } from "lucide-react";
 import Logo from "../assets/images/logo.svg";
 import AuthMenu from "./AuthMenu";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="flex justify-between items-center p-4 text-[hsl(var(--text-primary))]">
       <div className="flex items-center gap-2">
@@ -11,11 +14,15 @@ export default function Header() {
       </div>
 
       <div className="flex flex-col items-end relative">
-        <button aria-label="Open menu">
+        <button
+          className={`cursor-pointer ${isOpen ? "ring-2 ring-[hsl(var(--color-accent))]" : ""} rounded-md p-0.5`}
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Open menu"
+        >
           <Menu strokeWidth={3} />
         </button>
 
-        <AuthMenu />
+        {isOpen && <AuthMenu />}
       </div>
     </header>
   );
